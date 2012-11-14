0d0d76127d975105ff6e032ae3e44a35da6cc30e
Marcel Ruegenberg <dustlab@dustlab.com>
2012-11-14 17:12:41 +0100

    Tuned build.

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,32 @@
 import Distribution.Simple
-main = defaultMain
+
+import Distribution.Simple.Utils(warn)
+import Distribution.Verbosity(normal)
+import System.FilePath(getSearchPath,(</>))
+import System.Directory(doesFileExist,getPermissions,executable)
+import Control.Monad(filterM,unless)
+
+main = do
+  let hooks = simpleUserHooks {
+        preConf = \args cnfFlags -> do
+           info <- (preConf simpleUserHooks) args cnfFlags
+           checkPHC
+           return info
+        }
+  defaultMainWithHooks hooks
+
+checkPHC :: IO ()
+checkPHC = do
+  locations <- phcLocations
+  (if null locations then 
+     warn normal "No PHC binary found in PATH."
+   else do
+     p <- mapM getPermissions locations
+     unless (any executable p) $ warn normal "PHC file found, but not executable.")
+
+-- TODO: allow user to specify location and name of "phc" binary
+phcLocations :: IO [FilePath]
+phcLocations = do
+  paths <- getSearchPath
+  filterM doesFileExist $ map (</> "phc") paths
+  
