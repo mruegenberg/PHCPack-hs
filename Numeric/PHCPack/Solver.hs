a3c918c50accc3cb4a6009d85b12c2ed035c0cc5
Marcel Ruegenberg <dustlab@dustlab.com>
2012-12-16 18:18:12 +0100

    Improved API and docs.

--- Numeric/PHCPack/Solver.hs
+++ Numeric/PHCPack/Solver.hs
@@ -16,7 +16,8 @@ import Numeric.PHCPack.Types
 
 -- TODO: use unsafePerformIO?
 -- TODO: make this safer by a) enforcing that phc does not ask the user for anything b) using bracket to ensure that things are cleaned up
-solve :: (Num c, Ord c, Show c) => PolySystem c -> IO (Maybe Solution)
+-- | Solve a polynomial system
+solve :: (Num c, Ord c, Show c) => PolySystem c -> IO [Solution]
 solve polys = 
   withSystemTempFile "phc" $ \filePath handle -> do
     hPutStr handle (show polys)
@@ -28,46 +29,46 @@ solve polys =
       ExitFailure _ -> return Nothing
       ExitSuccess -> do
         solutionTxt <- readFile outFilePath
-        return $ fmap head $ parseSolutions (pack solutionTxt)
+        return $ parseSolutions (pack solutionTxt)
     removeFile outFilePath
-    return res
+    return $ maybe [] id res
         
   
 -- TODO: extract to proper test suite
 foo = PolySystem $ 
       [ Polynomial 
         [ Term 1 
-         [ TermFactor [Term 1 ["a"], Coefficient (-1)]
-         , TermFactor [Term 1 ["b"], Coefficient (-5)]
-         , TermFactor [Term 1 ["c"], Coefficient (-9)]
-         , TermFactor [Term 1 ["d"], Coefficient (-13)] ] 
+         [ Term 1 ["a"] <+> Coefficient (-1)
+         , Term 1 ["b"] <+> Coefficient (-5)
+         , Term 1 ["c"] <+> Coefficient (-9)
+         , Term 1 ["d"] <+> Coefficient (-13) ] 
          , Coefficient (-21)]
       , Polynomial 
         [ Term 1 
-          [ TermFactor [Term 1 ["a"], Coefficient (-2)]
-          , TermFactor [Term 1 ["b"], Coefficient (-6)]
-          , TermFactor [Term 1 ["c"], Coefficient (-10)]
-          , TermFactor [Term 1 ["f"], Coefficient (-17)] ] 
+          [ Term 1 ["a"] <+> Coefficient (-2)
+          , Term 1 ["b"] <+> Coefficient (-6)
+          , Term 1 ["c"] <+> Coefficient (-10)
+          , Term 1 ["f"] <+> Coefficient (-17) ] 
           , Coefficient (-22)]
       , Polynomial 
         [ Term 1 
-          [ TermFactor [Term 1 ["a"], Coefficient (-3)]
-          , TermFactor [Term 1 ["b"], Coefficient (-7)]
-          , TermFactor [Term 1 ["d"], Coefficient (-14)]
-          , TermFactor [Term 1 ["f"], Coefficient (-18)] ]  
+          [ Term 1 ["a"] <+> Coefficient (-3)
+          , Term 1 ["b"] <+> Coefficient (-7)
+          , Term 1 ["d"] <+> Coefficient (-14)
+          , Term 1 ["f"] <+> Coefficient (-18) ]  
           , Coefficient (-23)]
       , Polynomial 
         [ Term 1 
-          [ TermFactor [Term 1 ["a"], Coefficient (-4)]
-          , TermFactor [Term 1 ["c"], Coefficient (-11)]
-          , TermFactor [Term 1 ["d"], Coefficient (-15)]
-          , TermFactor [Term 1 ["f"], Coefficient (-19)] ]
+          [ Term 1 ["a"] <+> Coefficient (-4)
+          , Term 1 ["c"] <+> Coefficient (-11)
+          , Term 1 ["d"] <+> Coefficient (-15)
+          , Term 1 ["f"] <+> Coefficient (-19) ]
         , Coefficient (-24)]
       , Polynomial 
         [ Term 1 
-          [ TermFactor [Term 1 ["b"], Coefficient (-8)]
-          , TermFactor [Term 1 ["c"], Coefficient (-12)]
-          , TermFactor [Term 1 ["d"], Coefficient (-16)]
-          , TermFactor [Term 1 ["f"], Coefficient (-20)] ]
+          [ Term 1 ["b"] <+> Coefficient (-8)
+          , Term 1 ["c"] <+> Coefficient (-12)
+          , Term 1 ["d"] <+> Coefficient (-16)
+          , Term 1 ["f"] <+> Coefficient (-20) ]
         , Coefficient (-25)]
       ]
--- Numeric/PHCPack/Types.hs
+++ Numeric/PHCPack/Types.hs
@@ -1,22 +1,66 @@
-{-# LANGUAGE GADTs, StandaloneDeriving, OverloadedStrings #-}
+{-# LANGUAGE GADTs, StandaloneDeriving, OverloadedStrings, MultiParamTypeClasses, FlexibleInstances #-}
 
--- | The input format. `Show` instances result in a format compatible with http://homepages.math.uic.edu/~jan/PHCpack/node29.html.
--- | Take the following system where all polynomials should be equal:
--- | (a-1)*(b-5)*(c-9)*(d-13) - 21
--- | (a-2)*(b-6)*(c-10)*(f-17) - 22
--- | (a-3)*(b-7)*(d-14)*(f-18) - 23
--- | (a-4)*(c-11)*(d-15)*(f-19) - 24
--- | (b-8)*(c-12)*(d-16)*(f-20) - 25
--- |
--- | This corresponds to the following code:
--- |     PolySystem $ [
--- |     
+-- | The input format. `Show` instances result in a format compatible with <http://homepages.math.uic.edu/~jan/PHCpack/node29.html>.
+--   
+--   Take the following system where all polynomials should be equal:
+--   
+-- @
+--     (a-1)*(b-5)*(c-9)*(d-13) - 21
+--     (a-2)*(b-6)*(c-10)*(f-17) - 22
+--     (a-3)*(b-7)*(d-14)*(f-18) - 23
+--     (a-4)*(c-11)*(d-15)*(f-19) - 24
+--     (b-8)*(c-12)*(d-16)*(f-20) - 25
+-- @
+--   
+--   This corresponds to the following code:
+--   
+-- @
+--      PolySystem $ 
+--      [ Polynomial 
+--        [ Term 1 
+--         [ Term 1 [\"a\"] \<+\> Coefficient (-1)
+--         , Term 1 [\"b\"] \<+\> Coefficient (-5)
+--         , Term 1 [\"c\"] \<+\> Coefficient (-9)
+--         , Term 1 [\"d\"] \<+\> Coefficient (-13) ] 
+--         , Coefficient (-21)]
+--      , Polynomial 
+--        [ Term 1 
+--          [ Term 1 [\"a\"] \<+\> Coefficient (-2)
+--          , Term 1 [\"b\"] \<+\> Coefficient (-6)
+--          , Term 1 [\"c\"] \<+\> Coefficient (-10)
+--          , Term 1 [\"f\"] \<+\> Coefficient (-17) ] 
+--          , Coefficient (-22)]
+--      , Polynomial 
+--        [ Term 1 
+--          [ Term 1 [\"a\"] \<+\> Coefficient (-3)
+--          , Term 1 [\"b\"] \<+\> Coefficient (-7)
+--          , Term 1 [\"d\"] \<+\> Coefficient (-14)
+--          , Term 1 [\"f\"] \<+\> Coefficient (-18) ]  
+--          , Coefficient (-23)]
+--      , Polynomial 
+--        [ Term 1 
+--          [ Term 1 [\"a\"] \<+\> Coefficient (-4)
+--          , Term 1 [\"c\"] \<+\> Coefficient (-11)
+--          , Term 1 [\"d\"] \<+\> Coefficient (-15)
+--          , Term 1 [\"f\"] \<+\> Coefficient (-19) ]
+--        , Coefficient (-24)]
+--      , Polynomial 
+--        [ Term 1 
+--          [ Term 1 [\"b\"] \<+\> Coefficient (-8)
+--          , Term 1 [\"c\"] \<+\> Coefficient (-12)
+--          , Term 1 [\"d\"] \<+\> Coefficient (-16)
+--          , Term 1 [\"f\"] \<+\> Coefficient (-20) ]
+--        , Coefficient (-25)]
+--      ]
+-- @  
+-- 
+-- The syntax and API will likely be modified in the future to be simpler and more concise.
 module Numeric.PHCPack.Types (
      -- * Input
        PolySystem(..)
      , Polynomial(..)
      , Term(..)
-     , Factor(..)
+     , Factor((:^:)),(<+>)
      , Unknown
      -- * Solutions
      , Solution(..)
@@ -50,10 +94,13 @@ instance (Show c, Ord c) => Show (Polynomial c) where
             (Coefficient c) | c < 0 -> (Coefficient (abs c), "-")
             _                  -> (t2, "+")
 
--- | A term with a coefficient and a list of variables. `Term (-1.1) ["x1","x2"]` means (-1.1) * x1 * x2
+-- | A term with a coefficient and a list of variables. 'Term (-1.1) ["x1","x2"]' means (-1.1) * x1 * x2
 data Term c where
   Coefficient :: (Num c) => c -> Term c
   Term :: (Num c) => c -> Monomial c -> Term c
+  
+-- TODO: provide a Num instance so as to be able to provide just numbers instead of `Coefficient [number]`.
+--       this might, if done carefully, also lead to other nice things in the API
 
 deriving instance (Eq c) => Eq (Term c)
 
@@ -65,7 +112,7 @@ instance (Show c, Ord c) => Show (Term c) where
 -- | A product of factors
 type Monomial c = [Factor c]
 
--- | A factor. Either just a variable or a variable with an exponent. Use `:^:` and the isString instance to build factors.
+-- | A factor. Either just a variable or a variable with an exponent. Use `:^:` and the `IsString` instance to build factors.
 data Factor c where
   -- Build a factor with an exponent.
   (:^:) :: Unknown -> Natural -> Factor c
@@ -73,11 +120,23 @@ data Factor c where
   VarFactor :: Unknown -> Factor c
   -- Build a factor with a nested sequence of terms. The terms form a sum.
   TermFactor :: (Num c) => [Term c] -> Factor c
-  
-deriving instance (Eq c) => Eq (Factor c)
 
 instance IsString (Factor c) where
   fromString str = VarFactor str
+  
+class TermFactorBuilder a c where
+  -- | Concatenate terms to build a factor with a nested sequence of terms. The terms form a sum.
+  (<+>) :: (Num c) => Term c -> a c -> Factor c
+  
+infixr 5 <+>
+  
+instance TermFactorBuilder Term c where
+  t <+> t' = TermFactor [t,t']
+  
+instance TermFactorBuilder Factor c where
+  t <+> (TermFactor ts) = TermFactor (t : ts)
+  
+deriving instance (Eq c) => Eq (Factor c)
   
 instance (Show c, Ord c) => Show (Factor c) where
   show ((:^:) u e) = show u ++ "**" ++ "(" ++ show e ++ ")" -- FIXME: unknown whether the () are ok
@@ -100,6 +159,6 @@ type Unknown = String
 
 -- | A solution maps each unknown to a number
 data Solution where 
-  Solution :: (Num c, Show c) => Map Unknown c -> Solution
-  
+  Solution :: (Show c) => {solutionMap :: Map Unknown c} -> Solution
+    
 deriving instance Show Solution
